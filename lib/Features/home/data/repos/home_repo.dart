90e1cf02c5fd16b3eba36e3a4fbc26f5cd21cf4b
Anthony <anthonyghanem5@gmail.com>
2025-01-23 17:06:52 +0200

    implement fetch newest books

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -4,6 +4,6 @@ import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>>
-      fetchBestSellerBooks(); //the value on the left is the problem, the value on the right is the right type to be returned
+      fetchNewestBooks(); //the value on the left is the problem, the value on the right is the right type to be returned
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
