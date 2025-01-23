fbc213fadeef38b43b23e38ef3028640b6c0eecb
Anthony <anthonyghanem5@gmail.com>
2025-01-23 14:25:08 +0200

    create home repo methods and failure file

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,9 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>>
+      fetchBestSellerBooks(); //the value on the left is the problem, the value on the right is the right type to be returned
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
