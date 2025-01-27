053866ae0909aa480b009cbf1398bd824e5c318e
Anthony <anthonyghanem5@gmail.com>
2025-01-27 17:00:58 +0200

    launch url done

--- lib/Features/home/presentation/views/widgets/books_action.dart
+++ lib/Features/home/presentation/views/widgets/books_action.dart
@@ -1,17 +1,20 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/core/utils/functions/launch_url.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/widgets/custom_button.dart';
 
 class BooksAction extends StatelessWidget {
-  const BooksAction({super.key});
+  const BooksAction({super.key, required this.bookModel});
 
+  final BookModel bookModel;
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Row(
         children: [
-          Expanded(
+          const Expanded(
               child: CustomButton(
             text: '19.99 €',
             backgroundColor: Colors.white,
@@ -21,16 +24,29 @@ class BooksAction extends StatelessWidget {
           )),
           Expanded(
               child: CustomButton(
+            onPressed: () {
+              launchCustomUrl(context, bookModel.volumeInfo.previewLink);
+            },
             fontSize: 16,
-            text: 'Free Preview',
-            backgroundColor: Color(0xffEF8262),
+            text: getText(
+              bookModel,
+            ),
+            backgroundColor: const Color(0xffEF8262),
             textColor: Colors.white,
-            borderRadius: BorderRadius.only(
+            borderRadius: const BorderRadius.only(
                 topRight: Radius.circular(16),
                 bottomRight: Radius.circular(16)),
           )),
         ],
       ),
     );
+  }
+
+  String getText(BookModel bookModel) {
+    if (bookModel.volumeInfo.previewLink == null) {
+      return 'Not Available';
+    } else {
+      return 'Preview';
+    }
   }
 }
